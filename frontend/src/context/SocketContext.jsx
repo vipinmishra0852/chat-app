deaa2595ef9c2ec824fe66b9772dd2d13a208b35
vipinmishra0852 <vipinmishra0852@gmail.com>
2024-04-03 02:36:56 +0530

    3rd part completed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("http://localhost:5000", {
 				query: {
 					userId: authUser._id,
 				},
@@ -39,3 +39,4 @@ export const SocketContextProvider = ({ children }) => {
 
 	return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
 };
+ 
